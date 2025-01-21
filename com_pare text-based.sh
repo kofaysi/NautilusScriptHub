22774d9c393f32e9c4732c1dda4b8d1bf40dbff8
Milan <17022173+kofaysi@users.noreply.github.com>
2025-01-22 00:33:46 +0100

    Update com_pare text-based.sh
    Add function for input count check

--- com_pare text-based.sh
+++ com_pare text-based.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-# Check if exactly two file paths are provided
-if [ "$#" -ne 2 ]; then
-    zenity --error --text="Please select exactly two files to compare."
-    exit 1
-fi
+# Script to compare two files using kdiff3
+# Author: https://github.com/kofaysi/
+# Description: This script checks the number of input arguments and compares two files using kdiff3 if the correct number of arguments is provided.
+# Changelog:
+#   - [2025-01-22]: Add function for input count check
+
+# Function to check the number of input arguments
+check_input_count() {
+    local expected_count=$1
+    local actual_count=$#
+
+    if [ "$actual_count" -ne "$expected_count" ]; then
+        zenity --error --text="Expected $expected_count arguments, but got $actual_count. Please provide the correct number of inputs."
+        exit 1
+    fi
+}
+
+# Call the function to check input count, expecting 2 arguments
+check_input_count 2 "$@"
 
 # Assign the file paths to variables
 FILE1="$1"
@@ -12,4 +26,3 @@ FILE2="$2"
 
 # Launch kdiff3 to compare the files
 kdiff3 "$FILE1" "$FILE2"
-
