72d4ba8366ee2f3f900faaf7c9f10ea4e6b8ed84
Milan <milan.berta@e.email>
2025-01-25 23:03:59 +0100

    |updated the _git/commit and _push.sh\nlink the file to the parent folder.|

--- _repopulate-scripts-to-PATH.sh
+++ _repopulate-scripts-to-PATH.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# Adds Nautilus script directories to PATH if not already included.
+# Author: https://github.com/kofaysi/
+# Changelog:
+# - [2024-05-09]: Initial version
+# - [2025-01-25]: Added header
+
 for dir in ~/.local/share/nautilus/scripts/*; do
 	[[ ":$PATH:" != *":$dir:"* ]] && PATH="$PATH:$dir"
 done
