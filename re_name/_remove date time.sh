fdbea4352e11f47eba2efbd35473fa0741d8a2af
Milan <17022173+kofaysi@users.noreply.github.com>
2025-01-27 15:04:02 +0100

    Import common date script utils

--- re_name/_remove date time.sh
+++ re_name/_remove date time.sh
@@ -1,36 +1,7 @@
 #!/bin/bash
+source ~/.local/share/Nautilus/scripts/script_utils.sh
 
-# Iterate over each file or folder provided as input
-for item in "$@"; do
-    if [[ -e "$item" ]]; then  # Check if the item exists
-        # Extract the base name and directory path
-        base=$(basename "$item")
-        dir=$(dirname "$item")
-
-        # Determine if the item is a file or a directory
-        if [[ -f "$item" ]]; then
-            # Extract the filename and extension
-            filename="${base%.*}"
-            extension="${base##*.}"
-
-            # Remove the date, time, and trailing space from the beginning of the filename
-            new_filename="${filename#????-??-?? ???? }"
-
-            # Rename the file
-            mv "$item" "${dir}/${new_filename}.${extension}"
-
-            echo "Renamed: $item -> ${dir}/${new_filename}.${extension}"
-        elif [[ -d "$item" ]]; then
-            # Remove the date, time, and trailing space from the beginning of the directory name
-            new_dirname="${base#????-??-?? ???? }"
-
-            # Rename the directory
-            mv "$item" "${dir}/${new_dirname}"
-
-            echo "Renamed: $item -> ${dir}/${new_dirname}"
-        fi
-    else
-        echo "File or directory not found: $item"
-    fi
+for item in *; do
+    check_item_existence "$item" || continue
+    rename_item "$item" "${item#????-??-?? ???? }"
 done
-
