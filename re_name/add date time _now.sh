17400130acf05936f567c751e5285045527eb058
Milan <17022173+kofaysi@users.noreply.github.com>
2025-01-27 15:05:38 +0100

    Import common date script utils

--- re_name/add date time _now.sh
+++ re_name/add date time _now.sh
@@ -1,34 +1,9 @@
 #!/bin/bash
+source ~/.local/share/Nautilus/scripts/script_utils.sh
 
-# Get the current date and time in the desired format
-datetime=$(date +"%Y-%m-%d %H%M")
+datetime=$(get_current_datetime)
 
-# Iterate over each file or folder provided as input
-for item in "$@"; do
-    if [[ -f "$item" ]]; then  # Check if the item is a file
-        # Extract the filename and extension
-        filename=$(basename "$item")
-        extension="${filename##*.}"
-        filename="${filename%.*}"
-        
-        # Create the new filename with the current date and time
-        new_filename="${datetime} ${filename}"
-        
-        # Rename the file
-        mv "$item" "$new_filename.$extension"
-        
-        echo "Renamed: $item -> $new_filename.$extension"
-    elif [[ -d "$item" ]]; then  # Check if the item is a directory
-        # Extract the directory name
-        dirname=$(basename "$item")
-        
-        # Create the new directory name with the current date and time
-        new_dirname="${datetime} ${dirname}"
-        
-        # Rename the directory
-        mv "$item" "$new_dirname"
-        
-        echo "Renamed: $item -> $new_dirname"
-    fi
+for item in *; do
+    check_item_existence "$item" || continue
+    rename_item "$item" "${datetime} $item"
 done
-
