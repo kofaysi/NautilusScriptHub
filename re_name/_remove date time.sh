4f2d9104a077ffe2bc282b0eec7f4bb6b08bf795
Milan <milan.berta@e.email>
2025-01-28 09:10:57 +0100

    Fix: Fixed unescaped input Changed $@ to "$@" in "re_name/_remove date time.sh" "re_name/add _creation date.sh"  "re_name/add date time _now.sh"

--- re_name/_remove date time.sh
+++ re_name/_remove date time.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-source ~/.local/share/Nautilus/scripts/script_utils.sh
+source ~/.local/share/nautilus/scripts/script_utils.sh
 
-for item in *; do
+for item in "$@"; do
     check_item_existence "$item" || continue
     rename_item "$item" "${item#????-??-?? ???? }"
 done
--- re_name/add date time _now.sh
+++ re_name/add date time _now.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-source ~/.local/share/Nautilus/scripts/script_utils.sh
+source ~/.local/share/nautilus/scripts/script_utils.sh
 
 datetime=$(get_current_datetime)
 
-for item in *; do
+for item in "$@"; do
     check_item_existence "$item" || continue
     rename_item "$item" "${datetime} $item"
 done
